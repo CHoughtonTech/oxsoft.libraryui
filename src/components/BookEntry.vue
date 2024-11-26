c082cc62164dd80d0d94e7cd897b5a3629f11cc1
Curt Houghton <fatman_cah@hotmail.com>
2024-11-25 20:24:29 -0700

    LU-2: BookEntry
    Added book entry to help develop inputs needed, and the validations they require

---
+++ src/components/BookEntry.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { useVuelidate } from '@vuelidate/core'
+import { required } from '@vuelidate/validators'
+
+</script>
+<template>
+  <div
+    :class="$style['main-content']"
+  >
+    <form action="na" method="post">
+      <input type="text">
+    </form>
+  </div>
+</template>
+<style lang="scss" module>
+.main-content{
+  display: flex;
+  flex-direction: row;
+}
+</style>
